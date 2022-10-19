cfe149f114616656f1f07ddf37e404a4eb4824f2
viktoriya <malec1988@icloud.com>
2022-10-19 18:52:53 +0300

    Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ Task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте массив вещественных чисел(просто от минус бесконечности, до плюс бесконечности, с дробными не работаем). 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+int[] array = GetArray(5, -1000, 1000);// не нашла как ставить бесконечность
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{string.Join(",", array)}] = {difference(array)}");
+
+int difference(int[] arr)
+{
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    int diff = max - min;
+    return diff;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
